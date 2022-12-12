2b98702d8494e21289ee15c6f69a19d4e4784c1e
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-12 19:56:10 +0530

    here I added css in table.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React,{useState} from 'react';
+import './App.css'
 
 function Table(){
     const [table,settable]=useState('');
     const [list,setList]=useState([]);
     return(
-        <div>
-            <input  type="number" placeholder='Enter any number' onChange={(e)=>{
+        <div id='table'>
+          <h1>The Printing Table App.</h1>
+            <input  type="number"  placeholder='Enter any number' onChange={(e)=>{
                 settable(e.target.value)
             }}/>
             <button onClick={()=>{
               let newList=[]
+              let finalList=[]
               for(var i=1;i<11;i++){
-                    newList.push(table*i)
-                }
+                  newList.push([table ,'X',i,'=',table*i])
+              finalList.push(newList)
+              }
+
                 setList(newList)
             }}>Submit</button>
             {
